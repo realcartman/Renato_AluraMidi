d9b561aef660f433e3c95a4d7f584529cd7f33b7
Alucard <134976585+realcartman@users.noreply.github.com>
2023-09-25 20:06:29 -0300

    instrumento

--- main.js
+++ main.js
@@ -10,7 +10,13 @@ let contador = 0;
 //enquanto
 while (contador < listaDeTeclas.length) {
 
-   listaDeTeclas[contador].onclick = tocaSom;
+const instrumento = listaDeTeclas[contador].classList[1];
+
+console.log(instrumento);
+
+   listaDeTeclas[contador].onclick = function() {
+      tocaSom('#som_tecla_pom');
+   }
 
    contador = contador + 1;
 
