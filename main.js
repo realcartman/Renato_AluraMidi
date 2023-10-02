703d009c845a240c6c1447c17571eb17bea7da96
Alucard <134976585+realcartman@users.noreply.github.com>
2023-10-02 19:28:10 -0300

    ativa

--- main.js
+++ main.js
@@ -17,5 +17,9 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
       tocaSom(idAudio);
    }
 
+   tecla.onkeydown = function () {
+       tecla.classList.add('ativa')
+   }
+
 }
 
