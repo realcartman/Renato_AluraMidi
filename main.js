4468384598949dcc8475250c867b1ed666d94193
Alucard <134976585+realcartman@users.noreply.github.com>
2023-10-09 19:24:24 -0300

    abareta

--- main.js
+++ main.js
@@ -19,10 +19,13 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
    tecla.onkeydown = function (evento) {
 
-      console.log(evento.code == 'Space')
 
-       if (evento.code == 'Space') {
-       tecla.classList.add('ativa')
+       if (evento.code === 'Space') {
+       tecla.classList.add('ativa');
+       }
+
+       if (evento.code === 'Enter') {
+         tecla.classList.add('ativa');
        }
    }
 
